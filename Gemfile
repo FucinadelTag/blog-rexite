643c603507daa0e79836a5416598cdc46fcc6b87
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2014-10-20 22:23:45 +0200

    Prova

---
+++ Gemfile
@@ -0,0 +1,22 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'http://rubygems.org'
+
+gem "middleman", "~>3.3.2"
+gem "middleman-minify-html"
+gem "middleman-blog", "~> 3.5.1"
+
+# Prismic.io content repository
+gem "prismic.io", require: "prismic"
+gem "prismic_middleman", :git => 'git://github.com/FucinadelTag/FdtGems'
+
+# FdtTools
+gem "fdt_middleman_tools", ">= 1.0.0"
+
+# Live-reloading plugin
+gem "middleman-livereload", "~> 3.1.0"
+
+# For faster file watcher updates on Windows:
+gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+
+gem 'ejs'
---
+++ config.rb
@@ -0,0 +1,167 @@
+# Prismic
+#require 'prismic/lib/prismic.rb'
+#require 'prismic/lib/command/sync_blog.rb'
+###
+# Compass
+###
+
+compass_config do |config|
+  # Require any additional compass plugins here.
+  config.add_import_path "bower_components/foundation/scss"
+
+  # Set this to the root of your project when deployed:
+  config.http_path = "/"
+  config.css_dir = "stylesheets"
+  config.sass_dir = "stylesheets"
+  config.images_dir = "images"
+  config.javascripts_dir = "javascripts"
+
+  # You can select your preferred output style here (can be overridden via the command line):
+  output_style = :compressed
+
+  # To enable relative paths to assets via compass helper functions. Uncomment:
+  # relative_assets = true
+
+  # To disable debugging comments that display the original location of your selectors. Uncomment:
+  # line_comments = false
+
+  debug_info = true
+
+
+  # If you prefer the indented syntax, you might want to regenerate this
+  # project again passing --syntax sass, or you can uncomment this:
+  # preferred_syntax = :sass
+  # and then run:
+  # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+end
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (http://middlemanapp.com/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+# activate :livereload
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+activate :livereload
+activate :directory_indexes
+
+activate :blog do |blog|
+    # This will add a prefix to all links, template references and source paths
+    # blog.prefix = "blog"
+
+    # blog.permalink = "{year}/{month}/{day}/{title}.html"
+    # Matcher for blog source files
+    # blog.sources = "{year}-{month}-{day}-{title}.html"
+    # blog.taglink = "tags/{tag}.html"
+    # blog.layout = "main"
+    # blog.summary_separator = /(READMORE)/
+    # blog.summary_length = 250
+    # blog.year_link = "{year}.html"
+    # blog.month_link = "{year}/{month}.html"
+    # blog.day_link = "{year}/{month}/{day}.html"
+    blog.default_extension = ".erb"
+
+    blog.tag_template = "tag.html"
+    blog.calendar_template = "calendar.html"
+    blog.sources = "/blog/{category}/{year}-{month}-{day}-{title}.html"
+    blog.permalink = "/blog/{category}/{year}-{month}-{day}-{title}.html"
+
+    # Enable pagination
+    blog.paginate = true
+    blog.per_page = 10
+    blog.page_link = "page/{num}"
+
+    blog.custom_collections = {
+        category: {
+            link: '/blog/categories/{category}.html',
+            template: '/category.html'
+        }
+    }
+end
+
+page "/feed.xml", layout: false
+page "pagine/*", :layout => :page
+page "blog/*", :layout => :blog
+
+activate :relative_assets
+set :relative_links, false
+
+# Add bower's directory to sprockets asset path
+after_configuration do
+  @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+  sprockets.append_path File.join "#{root}", @bower_config["directory"]
+end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+set :partials_dir, 'partials'
+
+
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  activate :gzip
+
+  activate :minify_html
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
+
+activate :prismic_middleman do |f|
+  f.url = 'https://rexite.prismic.io/api'
+  f.new_article_template = "fdt_templates/blog.tt"
+end
